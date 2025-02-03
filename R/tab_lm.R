6130ce521d2f0411841d9c321612aa12ac25ac66
Sarah Schwartz <sarah.schwartz@usu.edu>
2025-02-03 12:47:55 -0700

    tidy up help for tab_lm

--- R/tab_lm.R
+++ R/tab_lm.R
@@ -1,4 +1,26 @@
-
+#' APA: flextable for ONElinear models
+#'
+#' @param x REQUIRED: 1 lm model, bare names
+#' @param var_labels Optional: Vector. Text replacements for model terms, "old" = "new"
+#' @param caption Optional: Text. Caption for the table
+#' @param p_note Optional: Text. (default = NULL) Significance note for APA table, If p_note = "apa" then the standard "* p < .05. ** p < .01. *** p < .001." will be used
+#' @param general_note Optional: Text. General note for footer of APA table
+#' @param fit Optional: vector. quoted names of fit statistics to include, can be: "r.squared", "adj.r.squared", "sigma", "statistic","p.value", "df", "logLik", "AIC", "BIC", "deviance", "df.residual", and "nobs"
+#' @param d Optional: Number. Digits after the decimal place
+#'
+#' @returns a flextable object
+#' @import gtsummary
+#' @import purrr
+#' @import magrittr
+#' @import tidyverse
+#' @import broom.helpers
+#' @export
+#'
+#' @examples
+#'
+#' m <- lm(dist ~ speed, cars)
+#' tab_lm(m)
+#'
 
 tab_lm <- function(x,
                    var_labels = NULL,
@@ -13,6 +35,7 @@ tab_lm <- function(x,
     coef() %>%
     length()
 
+  n_fit <- length(fit)
 
   get <- x %>%
     gtsummary::tbl_regression(intercept = TRUE,
@@ -46,14 +69,15 @@ tab_lm <- function(x,
 
   n_rows <- flextable::nrow_part(table, part = "body")
 
+  rows_fit <- (n_rows - n_fit + 1):(n_rows)
+
   table <- table %>%
     apaSupp::theme_apa(caption = caption,
                        p_note = p_note,
                        general_note = general_note) %>%
-    flextable::hline(i = n_rows - 2) %>%
+    flextable::hline(i = n_rows - n_fit) %>%
     flextable::italic(part = "header", i = 1) %>%
-    flextable::italic(part = "body", i = c(n_rows - 1,
-                                           n_rows - 0))
+    flextable::italic(part = "body", i = rows_fit)
 
   if(!is.null(var_labels)){
     table <- table %>%
