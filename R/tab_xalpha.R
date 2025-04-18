6e4b40b8005adb7953d0e6ad627c42f5b405c1a4
Sarah Schwartz <sarah.schwartz@usu.edu>
2025-04-18 16:49:40 -0600

    fixing tab_xapha more

--- R/tab_xalpha.R
+++ R/tab_xalpha.R
@@ -82,6 +82,7 @@ tab_xalpha <- function(df,
     flextable::as_i(    "Note. "),
     flextable::as_i(    "N"),
     flextable::as_chunk(glue::glue(" = {n}. ")),
+    flextable::as_i(    "n"),   " = complete observations; ",
     flextable::as_i(    "Std"), " = standardized; ",
     flextable::as_i(    "Mdn"), " = median. ",
     flextable::as_chunk(general_note)
@@ -91,25 +92,31 @@ tab_xalpha <- function(df,
   x <- dfc %>%
     tidyr::nest(data = c(!!item, !!value, !!id)) %>%
     dplyr::mutate(data_wide = data %>%
-                    map(~ data.frame(.x) %>%
-                          tidyr::pivot_wider(names_from = !!item,
-                                             names_prefix = "item_",
-                                             values_from = !!value))) %>%
-    dplyr::mutate(lo = purrr::map_dbl(data, ~ min(.x %>% dplyr::select(!!value)))) %>%
-    dplyr::mutate(hi = purrr::map_dbl(data, ~ max(.x %>% dplyr::select(!!value)))) %>%
+                    purrr::map(~ data.frame(.x) %>%
+                                 tidyr::pivot_wider(names_from = !!item,
+                                                    names_prefix = "item_",
+                                                    values_from = !!value))) %>%
     dplyr::mutate(psych_alpha = purrr::map(data_wide,
                                            ~ data.frame(.x) %>%
                                              dplyr::select(-!!id) %>%
                                              psych::alpha(discrete = FALSE))) %>%
+    dplyr::mutate(nids = psych_alpha %>%
+                    purrr::map(~ min(.x$item.stats$n)) %>%
+                    as.character()) %>%
     dplyr::mutate(nvar = psych_alpha %>%
                     purrr::map_dbl(~.x$nvar) %>%
                     as.character()) %>%
+    dplyr::mutate(lo = purrr::map_dbl(data, ~ min(.x %>% dplyr::select(!!value)))) %>%
+    dplyr::mutate(hi = purrr::map_dbl(data, ~ max(.x %>% dplyr::select(!!value)))) %>%
+    dplyr::mutate(range = paste0(floor(lo), " - ", ceiling(hi))) %>%
     dplyr::mutate(a_tot = purrr::map(psych_alpha,
                                      ~ .x$total)) %>%
     tidyr::unnest(a_tot) %>%
-    dplyr::mutate(range = paste0(floor(lo), " - ", ceiling(hi))) %>%
+    dplyr::mutate(across(c(raw_alpha, std.alpha, `G6(smc)`, average_r, median_r),
+                         ~ apaSupp::p_num(.x, d = d + 1, stars = FALSE))) %>%
     dplyr::select(!!measure,
                   !!domain,
+                  "n" = nids,
                   "Num\nItems" = nvar,
                   "Value\nRange" = range,
                   "Chronbach's\nAlpha_Raw" = raw_alpha,
@@ -128,8 +135,8 @@ tab_xalpha <- function(df,
     y <- x  %>%
       flextable::as_grouped_data(groups = names(x)[1]) %>%
       flextable::as_flextable(hide_grouplabel = TRUE) %>%
-      flextable::bold(i = grp_lines, part = "body") %>%
-      flextable::merge_h_range(i = grp_lines, j1 = 1, j2 = 8, part = "body")
+      flextable::bold(part = "body", i = grp_lines) %>%
+      flextable::merge_h_range(part = "body", i = grp_lines, j1 = 1, j2 = 9)
   } else {
     grp_lines <- NULL
     y <- x %>% flextable::flextable()
@@ -141,12 +148,16 @@ tab_xalpha <- function(df,
                        main_note    = main_note,
                        d            = d) %>%
     flextable::line_spacing(space = 1, part = "header") %>%
-    flextable::italic( part = "header", i = 2, j = c(4, 5, 7, 8)) %>%   #added
-    flextable::align(  part = "all",  j = -1,      align = "center") %>%
-    flextable::padding(part = "all",  j = c(6),    padding.left =  5, padding.right = 5) %>%
-    flextable::padding(part = "all",  j = c(4, 7), padding.left = 10, padding.right = 1) %>%
-    flextable::padding(part = "all",  j = c(5, 8), padding.left =  1, padding.right = 10) %>%
+    flextable::italic( part = "header", i = 2, j = c(2, 5, 6, 8, 9)) %>%   #added
+    flextable::align(  part = "all",  j = c(1, 6, 9),    align = "left") %>%
+    flextable::align(  part = "all",  j = c(5, 8),       align = "right") %>%
+    flextable::align(  part = "all",  j = c(2, 3, 4, 7), align = "center") %>%
+    flextable::align(  part = "header", i = 1, j = c(5:6, 8:9), align = "center") %>%
+    flextable::hline(  part = "header", i = 1, j = c(5:6, 8:9)) %>%
+    flextable::valign( part = "header", j = 1:4, valign = "bottom") %>%
+    flextable::compose(part = "header", j = 1, value = flextable::as_paragraph(NA)) %>%
     flextable::autofit()
+
 
   return(table)
 
